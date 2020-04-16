fc850655f5b31b3f15ffbf346a46b61c83cf0f37
meghapatel99 <tuh02762@temple.edu>
2020-04-16 13:14:04 -0400

    making sure nothings lost

--- TP_WebAPI/Controllers/InteractionsController.cs
+++ TP_WebAPI/Controllers/InteractionsController.cs
@@ -28,7 +28,7 @@ namespace TP_WebAPI.Controllers
             objInsertPref.Parameters.AddWithValue("@dislikes", p.mDislikes);
             objInsertPref.Parameters.AddWithValue("@blocks", p.mBlocks);
             int result = objDB.DoUpdateUsingCmdObj(objInsertPref, out string error);
-  
+
             return result;
         }
 
@@ -43,6 +43,21 @@ namespace TP_WebAPI.Controllers
             return result;
         }
 
+        [HttpPut("blockUser")]
+        public int BlockUser([FromBody] IDictionary<string, string> newValues)
+        { // removes all interactions between 2 users when blocked
+            int userID = Convert.ToInt16(newValues["userID"]);
+            int memID = Convert.ToInt16(newValues["memID"]);
+
+            SqlCommand objBlock = new SqlCommand();
+            objBlock.CommandType = CommandType.StoredProcedure;
+            objBlock.CommandText = "TP_Block";
+            objBlock.Parameters.AddWithValue("@userID", userID);
+            objBlock.Parameters.AddWithValue("@memID", memID);
+            int result = objDB.DoUpdateUsingCmdObj(objBlock, out string error);
+            return result;
+        }
+
         [HttpPut("updatePreferences")]
         public int UpdatePreferences([FromBody] Preferences p)
         { // updates preferences for user
@@ -54,9 +69,143 @@ namespace TP_WebAPI.Controllers
             objUpdatePref.Parameters.AddWithValue("@dislikes", p.mDislikes);
             objUpdatePref.Parameters.AddWithValue("@blocks", p.mBlocks);
             int result = objDB.DoUpdateUsingCmdObj(objUpdatePref, out string error);
- 
+
             return result;
         }
 
+
+        [HttpPost("addDateReq")]
+        public DataSet AddDateReq([FromBody] IDictionary<string, string> vals)
+        { // adds a date request to the db
+            int sendingID = Convert.ToInt16(vals["sendingID"]);
+            int recID = Convert.ToInt16(vals["recID"]);
+            string message = vals["message"];
+            DateTime now = DateTime.Now;
+
+            SqlCommand objDateReq = new SqlCommand();
+            objDateReq.CommandType = CommandType.StoredProcedure;
+            objDateReq.CommandText = "TP_SendDateRequest";
+            objDateReq.Parameters.AddWithValue("@userID", sendingID);
+            objDateReq.Parameters.AddWithValue("@memID", recID);
+            objDateReq.Parameters.AddWithValue("@now", now);
+            objDateReq.Parameters.AddWithValue("@message", message);
+            DataSet result = objDB.GetDataSetUsingCmdObj(objDateReq);
+            return result;
+        } // end add date req
+
+        [HttpGet("getAllDates/{userID}")]
+        public DataSet GetAllDates(int userID)
+        { // gets all dating reqs
+            SqlCommand objGetDates = new SqlCommand();
+            objGetDates.CommandType = CommandType.StoredProcedure;
+            objGetDates.CommandText = "TP_GetAllDates";
+            objGetDates.Parameters.AddWithValue("@userID", userID);
+            DataSet result = objDB.GetDataSetUsingCmdObj(objGetDates);
+            return result;
+        } // end get all dates
+
+        [HttpPut("deleteDateReq")]
+        public int DeleteDateRequest([FromBody] IDictionary<string, string> vals)
+        { // deletes a dating request
+            int sendingID = Convert.ToInt16(vals["sendingID"]);
+            int recievingID = Convert.ToInt16(vals["recID"]);
+
+            SqlCommand objDelReq = new SqlCommand();
+            objDelReq.CommandType = CommandType.StoredProcedure;
+            objDelReq.CommandText = "TP_DeleteDateReq";
+            objDelReq.Parameters.AddWithValue("@sendingID", sendingID);
+            objDelReq.Parameters.AddWithValue("@recID", recievingID);
+            int res = objDB.DoUpdateUsingCmdObj(objDelReq, out string err);
+            return res;
+        } // end delete date req
+
+        [HttpPut("acceptReq")]
+        public int AccceptReq([FromBody] IDictionary<string, string> vals)
+        {
+            int sendingID = Convert.ToInt16(vals["sendingID"]);
+            int recievingID = Convert.ToInt16(vals["recID"]);
+
+            SqlCommand objAcceptReq = new SqlCommand();
+            objAcceptReq.CommandType = CommandType.StoredProcedure;
+            objAcceptReq.CommandText = "TP_AcceptReq";
+            objAcceptReq.Parameters.AddWithValue("@sendingID", sendingID);
+            objAcceptReq.Parameters.AddWithValue("@recID", recievingID);
+
+            int res = objDB.DoUpdateUsingCmdObj(objAcceptReq, out string err);
+            return res;
+        } // end accept req
+
+        [HttpPut("denyReq")]
+        public int DenyReq([FromBody] IDictionary<string, string> vals)
+        {
+            int sendingID = Convert.ToInt16(vals["sendingID"]);
+            int recievingID = Convert.ToInt16(vals["recID"]);
+
+            SqlCommand objDenyReq = new SqlCommand();
+            objDenyReq.CommandType = CommandType.StoredProcedure;
+            objDenyReq.CommandText = "TP_AcceptReq";
+            objDenyReq.Parameters.AddWithValue("@senderID", sendingID);
+            objDenyReq.Parameters.AddWithValue("@recID", recievingID);
+
+            int res = objDB.DoUpdateUsingCmdObj(objDenyReq, out string err);
+            return res;
+        }
+
+        [HttpGet("getAcceptedDates/{userID}")]
+        public DataSet GetAcceptedReqs(int userID)
+        {
+            SqlCommand objAccepted = new SqlCommand();
+
+            objAccepted.CommandType = CommandType.StoredProcedure;
+            objAccepted.CommandText = "TP_GetAcceptedDates";
+            objAccepted.Parameters.AddWithValue("@userID", userID);
+
+            DataSet res = objDB.GetDataSetUsingCmdObj(objAccepted);
+            return res;
+        } // end getacceptedreqs
+
+        [HttpPost("insertDate")]
+        public int InsertDate([FromBody] IDictionary<string, string> vals)
+        { // this inserts a date to the db
+            int sendingID = Convert.ToInt16(vals["sendingID"]);
+            int recID = Convert.ToInt16(vals["recID"]);
+            DateTime dt = Convert.ToDateTime(vals["dt"]);
+            string location = vals["location"];
+            string desc = vals["desc"];
+
+            SqlCommand objInsertDt = new SqlCommand();
+            objInsertDt.CommandType = CommandType.StoredProcedure;
+            objInsertDt.CommandText = "TP_InsertDateDetails";
+            objInsertDt.Parameters.AddWithValue("@sendingID", sendingID);
+            objInsertDt.Parameters.AddWithValue("@recID", recID);
+            objInsertDt.Parameters.AddWithValue("@dt", dt);
+            objInsertDt.Parameters.AddWithValue("@location", location);
+            objInsertDt.Parameters.AddWithValue("@description", desc);
+
+            int res = objDB.DoUpdateUsingCmdObj(objInsertDt, out string err);
+            return res;
+        } // end insert date
+
+        [HttpPut("updateDate/")]
+        public int UpdateDate([FromBody] IDictionary<string, string> vals)
+        { // updates date details when the user edits it
+            int sendingID = Convert.ToInt16(vals["sendingID"]);
+            int recID = Convert.ToInt16(vals["recID"]);
+            DateTime dt = Convert.ToDateTime(vals["dt"]);
+            string location = vals["location"];
+            string desc = vals["desc"];
+
+            SqlCommand objUpdateDate = new SqlCommand();
+            objUpdateDate.CommandType = CommandType.StoredProcedure;
+            objUpdateDate.CommandText = "TP_UpdateDate";
+            objUpdateDate.Parameters.AddWithValue("@sendingID", sendingID);
+            objUpdateDate.Parameters.AddWithValue("@recID", recID);
+            objUpdateDate.Parameters.AddWithValue("@dt", dt);
+            objUpdateDate.Parameters.AddWithValue("@location", location);
+            objUpdateDate.Parameters.AddWithValue("@description", desc);
+
+            int res = objDB.DoUpdateUsingCmdObj(objUpdateDate, out string err);
+            return res;
+        } // end update date
     }
 }
--- TermProject/Dates.aspx.cs
+++ TermProject/Dates.aspx.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace TermProject
@@ -11,9 +17,272 @@ namespace TermProject
     {
         string interactionsWebAPI = "https://localhost:44375/api/datingservice/interactions/";
         string profileWebAPI = "https://localhost:44375/api/datingservice/profile/";
+        int userID;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["UserID"] == null) Response.Redirect("LogIn.aspx?target=Dates");
+            if (Session["UserID"] == null) Response.Redirect("Default.aspx");
+            else
+            {
+                userID = Convert.ToInt32(Session["UserID"].ToString());
+
+                if (!IsPostBack)
+                {
+                    Boolean p = true; Boolean a = true; Boolean s = true; Boolean d = true;
+                    bind(p, a, s, d);
+                }
+            }
+
+        } // end page load
+
+        protected void bind(Boolean pend, Boolean approve, Boolean Sch, Boolean dates)
+        {
+            WebRequest request; WebResponse response; Stream theDataStream;
+            StreamReader reader; string data; JavaScriptSerializer js;
+            if (pend || approve)
+            {
+                request = WebRequest.Create(interactionsWebAPI + "getAllDates/" + userID);
+                response = request.GetResponse();
+                theDataStream = response.GetResponseStream();
+                reader = new StreamReader(theDataStream);
+                data = reader.ReadToEnd();
+                reader.Close(); response.Close();
+
+                js = new JavaScriptSerializer();
+                DataSet dsPending = JsonConvert.DeserializeObject<DataSet>(data);
+                DataTable pending = dsPending.Tables[0];
+                rptPending.DataSource = pending; rptPending.DataBind();
+
+                DataTable approveDT = dsPending.Tables[1];
+                rptAcceptReqs.DataSource = approveDT; rptAcceptReqs.DataBind();
+
+            }
+            if (Sch || dates)
+            { // display the dates that need to be scheduled
+                request = WebRequest.Create(interactionsWebAPI + "getAcceptedDates/" + userID);
+                response = request.GetResponse();
+                theDataStream = response.GetResponseStream();
+                reader = new StreamReader(theDataStream);
+                data = reader.ReadToEnd();
+                reader.Close(); response.Close();
+
+                js = new JavaScriptSerializer();
+                DataSet dsAccepted = JsonConvert.DeserializeObject<DataSet>(data);
+                DataTable a = dsAccepted.Tables[0];
+
+                lvSchedule.DataSource = a;
+                //String[] names = new String[2];  names[0] = "userID"; names[1] = "userName";
+                //  lvSchedule.DataKeyNames = names;
+                lvSchedule.DataBind();
+
+                DataTable scheduledDates = dsAccepted.Tables[1];
+                String[] keys = new string[1]; keys[0] = "userName";
+                lvPlannedDates.DataSource = scheduledDates; lvPlannedDates.DataBind();
+            }
+
         }
-    }
-}
+
+        protected void lblDeleteReq_Command(object sender, CommandEventArgs e)
+        { // this will delete the date request
+            int memID = Convert.ToInt32(e.CommandName);
+            IDictionary<string, string> newValues = new Dictionary<string, string>
+            {
+                ["sendingID"] = userID.ToString(),
+                ["recID"] = memID.ToString(),
+            };
+
+            sendReq(newValues, "deleteDateReq");
+
+            Boolean p = true; Boolean a = false; Boolean s = false; Boolean d = false;
+            bind(p, a, s, d); // rebind that repeater
+        }
+
+        protected void lbAccept_Command(object sender, CommandEventArgs e)
+        { // accept a dating request
+            int memID = Convert.ToInt32(e.CommandName);
+            IDictionary<string, string> newValues = new Dictionary<string, string>
+            {
+                ["sendingID"] = memID.ToString(),
+                ["recID"] = userID.ToString(),
+            };
+
+            sendReq(newValues, "acceptReq");
+
+            Boolean p = true; Boolean a = true; Boolean s = true; Boolean d = false;
+            bind(p, a, s, d); // rebind that repeater
+        }
+
+        protected void lbDeny_Command(object sender, CommandEventArgs e)
+        { // deny request
+            int memID = Convert.ToInt32(e.CommandName);
+            IDictionary<string, string> newValues = new Dictionary<string, string>
+            {
+                ["sendingID"] = memID.ToString(),
+                ["recID"] = userID.ToString(),
+            };
+
+            sendReq(newValues, "denyReq");
+
+            Boolean p = false; Boolean a = true; Boolean s = false; Boolean d = false;
+            bind(p, a, s, d); // rebind that repeater
+        } // end deny
+
+        protected void lbIgnore_Command(object sender, CommandEventArgs e)
+        {
+
+        }
+
+        protected void sendReq(IDictionary<string, string> vals, string method)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            String jsonValues = js.Serialize(vals);
+
+            WebRequest request = WebRequest.Create(interactionsWebAPI + method + "/");
+            request.Method = "PUT";
+            request.ContentLength = jsonValues.Length;
+            request.ContentType = "application/json";
+
+            StreamWriter writer = new StreamWriter(request.GetRequestStream());
+            writer.Write(jsonValues);
+            writer.Flush();
+            writer.Close();
+
+            WebResponse response = request.GetResponse();
+            Stream theDataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(theDataStream);
+            String data = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+        }
+
+        protected void lbGotoProf_Command(object sender, CommandEventArgs e)
+        { // takes you to that person's profile
+            int memID = Convert.ToInt32(e.CommandName);
+            Response.Redirect("MemberProfile.aspx?memberID=" + memID);
+        }
+
+        protected void lbGotoProf2_Command(object sender, CommandEventArgs e)
+        {// takes you to that person's profile
+            int memID = Convert.ToInt32(e.CommandName);
+            Response.Redirect("MemberProfile.aspx?memberID=" + memID);
+        }
+
+        protected void btnShowDate_Command(object sender, CommandEventArgs e)
+        {
+            divDates.Attributes.Add("style", "display:flex");
+            int memID = Convert.ToInt32(e.CommandName);
+            string name = "";
+            Session["memID"] = memID;
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        { // hides the set up date card
+            divDates.Attributes.Add("style", "display:none");
+        }
+
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        { // saves the 
+            int memberID = Convert.ToInt16(Session["memID"]);
+            string dt = txtWhen.Text;
+            string location = txtLocation.Text;
+            string desc = txtDesc.Text;
+
+            // remove validation class 
+            txtWhen.CssClass = txtWhen.CssClass.Replace("is-invalid", "").Trim();
+            txtLocation.CssClass = txtLocation.CssClass.Replace("is-invalid", "").Trim();
+            txtDesc.CssClass = txtDesc.CssClass.Replace("is-invalid", "").Trim();
+
+            // validate
+            Boolean trigger = false;
+            if (dt == "")
+            {
+                trigger = true;
+                txtWhen.CssClass += " is-invalid";
+            }
+            if (location == "")
+            {
+                trigger = true;
+                txtLocation.CssClass += " is-invalid";
+            }
+            if (desc == "")
+            {
+                trigger = true;
+                txtDesc.CssClass += " is-invalid";
+            }
+
+            if (!trigger)
+            { // if everything is entered - update the db
+                IDictionary<string, string> newValues = new Dictionary<string, string>
+                {
+                    ["sendingID"] = userID.ToString(),
+                    ["recID"] = memberID.ToString(),
+                    ["dt"] = dt,
+                    ["location"] = location,
+                    ["desc"] = desc
+                };
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string values = js.Serialize(newValues);
+
+                WebRequest request = WebRequest.Create(interactionsWebAPI + "insertDate/");
+                request.Method = "POST";
+                request.ContentLength = values.Length;
+                request.ContentType = "application/json";
+
+                StreamWriter writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(values);
+                writer.Flush();
+                writer.Close();
+
+                WebResponse response = request.GetResponse();
+                Stream theDataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(theDataStream);
+                String data = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+
+                Boolean p = false; Boolean a = false; Boolean s = true; Boolean d = true;
+                bind(p, a, s, d); // rebind that repeater
+
+                divDates.Attributes.Add("style", "display:none");
+
+            }
+
+        } // end save button click
+
+        protected void lbSaveChanges_Command(object sender, CommandEventArgs e)
+        { // save changes to the plans
+            int memberID = Convert.ToInt16(e.CommandName);
+            Boolean trigger = false;
+
+            if (!trigger)
+            {
+                // sendReq(newVals, "updateDate");
+            }
+
+        }
+
+        protected void lvPlannedDates_ItemEditing(object sender, ListViewEditEventArgs e)
+        {
+            lvPlannedDates.EditIndex = e.NewEditIndex;
+            Boolean p = false; Boolean a = false; Boolean s = false; Boolean d = true;
+            bind(p, a, s, d);
+        }
+
+        protected void lvPlannedDates_ItemCanceling(object sender, ListViewCancelEventArgs e)
+        {
+            lvPlannedDates.EditIndex = -1;
+            Boolean p = false; Boolean a = false; Boolean s = false; Boolean d = true;
+            bind(p, a, s, d);
+        }
+
+        protected void lvPlannedDates_ItemUpdating(object sender, ListViewUpdateEventArgs e)
+        {
+            int rowIndex = e.ItemIndex;
+            string test = lvPlannedDates.Items[rowIndex].ToString();
+            Boolean p = false; Boolean a = false; Boolean s = false; Boolean d = true;
+            bind(p, a, s, d);
+        }
+    } // end class
+} // end namespace
